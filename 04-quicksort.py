79b8e0c0e14f15afb694a039453b8ec0561c57d7
Alexander Kramer <alexander.kramer@timetoact.de>
2025-11-10 08:37:34 +0100

    :alembic: chapter 4 - quick sort - max exercise

--- 04-quicksort.py
+++ 04-quicksort.py
@@ -1,28 +1,15 @@
-def my_sum(arr):
-    # return my_sum_rec(arr) --> can not even process arr with 1_000 entries
-    # return my_sum_loop(arr)
-    return sum(arr)
+def my_max(arr, index=0, max_value=0):
+    if index == len(arr):
+        return max_value
 
+    curr = arr[index]
+    if curr > max_value:
+        max_value = curr
 
-def my_sum_rec(arr, index=0, total=0):
-    if len(arr) == 0:
-        return 0
-    if len(arr) == index:
-        return total
-
-    total += arr[index]
     index += 1
-
-    return my_sum_rec(arr, index, total)
-
-
-def my_sum_loop(arr):
-    total = 0
-    for x in arr:
-        total += x
-    return total
+    return my_max(arr, index, max_value)
 
 
 if __name__ == '__main__':
-    my_list = range(1_000_000_000)  # 210
-    print(my_sum(my_list))
+    my_list = [20, 33, 2, 56, 12, 1, 0]
+    print(my_max(my_list))
