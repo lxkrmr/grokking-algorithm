91f16f58a9199c79bdb85bfbcfa9be6132796062
Alexander Kramer <alexander.kramer@timetoact.de>
2025-11-11 12:38:59 +0100

    :sparkles: Chapter 10 - greedy algorithm

---
+++ greedy.py
@@ -0,0 +1,30 @@
+def main():
+    states_needed = {"mt", "wa", "or", "id", "nv", "ut", "ca", "az"}
+
+    stations = {
+        "kone": {"id", "nv", "ut"},
+        "ktwo": {"wa", "id", "mt"},
+        "kthree": {"or", "nv", "ca"},
+        "kfour": {"nv", "ut"},
+        "kfive": {"ca", "az"},
+    }
+
+    final_stations = set()
+
+    while states_needed:
+        best_station = None
+        states_covered = set()
+        for station, stated_for_station in stations.items():
+            covered = states_needed & stated_for_station
+            if len(covered) > len(states_covered):
+                best_station = station
+                states_covered = covered
+
+        final_stations.add(best_station)
+        states_needed -= states_covered
+
+    print(final_stations)
+
+
+if __name__ == '__main__':
+    main()
